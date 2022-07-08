621ccf28cafa2b8fa9169c412ce110484244ac30
imviz <vishnuedappatt@gmail.com>
2022-07-08 09:27:34 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Banner from './Components/Banner/Banner';
 import NavBar from './Components/NaveBar/NavBar';
 import RowPost from './Components/RowPost/RowPost';
-import {originals,horror} from './urls'
+import {originals,horror,action} from './urls'
 
 function App() {
   return (
@@ -11,7 +11,9 @@ function App() {
       <NavBar/>
       <Banner />
       <RowPost url={originals} title='Netflix Originals'/>
-      <RowPost url={horror} title='action' isSmall/> 
+      <RowPost url={action} title='action' isSmall/> 
+      <RowPost url={horror} title='horror' isSmall/> 
+      
     </div>
   
   );
--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,4 @@
 import {API_KEY} from './constants/constants'
 export const originals=`discover/tv?api_key=${API_KEY}&with_networks=213`
 export const horror=`discover/movie?api_key=${API_KEY}&with_genres=27`
+export const action=`discover/movie?api_key=${API_KEY}&with_genres=28`
